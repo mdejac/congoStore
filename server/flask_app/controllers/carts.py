9d57b698655dd610555831fd58c7f0ef424c89cd
mdejac <dejacm@gmail.com>
2023-08-21 04:59:53 -0400

    Changed cart api to work with user_id Added flask templates

--- server/flask_app/controllers/carts.py
+++ server/flask_app/controllers/carts.py
@@ -5,3 +5,10 @@ from flask_app.models.user import User
 from flask_app.models.product import Product
 from flask_app.models.review import Review
 from flask_app import app
+
+@app.route('/carts/view/<int:user_id>')
+def view_cart(user_id):
+    if 'user_id' in session:
+        cart = Cart.view_cart_by_user_id(user_id)
+        return render_template('cart_view.html', cart=cart)
+    return redirect('/')
