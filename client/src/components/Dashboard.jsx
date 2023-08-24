3b8fa9045f6d121f27a1c9a81e613e0068d95720
mdejac <dejacm@gmail.com>
2023-08-24 01:36:06 -0400

    Update ProductScroll and Dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,19 +1,19 @@
 import React, {useEffect, useState} from "react";
-import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 import ProductScroll from "./ProductScroll";
 
-// import SearchBar from "./SearchBar";
-// import ProductList from "./ProductList";
-
 function Dashboard() {
     const navigate = useNavigate();
     const [user, setUser] = useState({});
-    const [allProducts, setAllProducts] = useState([]);
-    const [allProductsByCategory1, setAllProductsByCategory1] = useState([]);
-    const [allProductsByCategory2, setAllProductsByCategory2] = useState([]);
+
+    const categories = {
+        1 : 'Back to School',
+        2 : 'Computers',
+        3 : 'Electronics',
+        4 : 'Furniture'
+    }
 
     useEffect(() => {
         const loggedInUser = localStorage.getItem("user");
@@ -24,43 +24,12 @@ function Dashboard() {
             navigate("/");
         }
       }, []);
-
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/api/products')
-          .then(response => {
-            setAllProducts(response.data);
-          })
-          .catch(error => {
-            console.error('Error fetching data:', error);
-          });
-    }, []);
-
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/api/products/category/furniture')
-          .then(response => {
-            setAllProductsByCategory1(response.data);
-          })
-          .catch(error => {
-            console.error('Error fetching data:', error);
-          });
-    }, []);
-    
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/api/products/category/back to school')
-          .then(response => {
-            setAllProductsByCategory2(response.data);
-          })
-          .catch(error => {
-            console.error('Error fetching data:', error);
-          });
-    }, []);
-
-
-    
+   
     return (
-        <div className="dashboard">
-            <ProductScroll searchType='Furniture' products={allProductsByCategory1}/>
-            <ProductScroll searchType='Back To School' products={allProductsByCategory2}/>
+        <div className="dashboard mt-5">
+            {Object.keys(categories).map((categoryId) => (
+                <ProductScroll key={categoryId} searchType={categories[categoryId]}/>
+            ))}
         </div>
     );
 }
